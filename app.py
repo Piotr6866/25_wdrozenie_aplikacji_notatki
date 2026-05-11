2679cf998c5004e91cbde80d03513daf6c448fe7
PiotrK <Piotr_Kotecki@hotmail.com>
2026-05-12 00:29:31 +0200

    Eliminating error

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ env = dotenv_values(".env")
 if 'QDRANT_URL' in st.secrets:
     env['QDRANT_URL'] = st.secrets['QDRANT_URL']
 if 'QDRANT_API_KEY' in st.secrets:
-    env['QDstreamlit run app.py                      RANT_API_KEY'] = st.secrets['QDRANT_API_KEY']
+    env['QDRANT_API_KEY'] = st.secrets['QDRANT_API_KEY']
 ###
 
 EMBEDDING_MODEL = "text-embedding-3-large"
